b2f390e92db8f5bceaee7c62b41ee8739439cbe4
Sathish_ofc <sathishkumarashram25800@gmail.com>
2022-08-27 15:17:09 +0530

    added systemchrome >>Potrait orientation

--- lib/screens/boarding_screen.dart
+++ lib/screens/boarding_screen.dart
@@ -2,127 +2,159 @@
 
 import 'package:dots_indicator/dots_indicator.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:vendbot/screens/landing_screen.dart';
 
 class Boardingscreen extends StatefulWidget {
   const Boardingscreen({Key? key}) : super(key: key);
-  static const String id="BoardingScreen";
+  static const String id = "BoardingScreen";
 
   @override
   State<Boardingscreen> createState() => _BoardingscreenState();
 }
 
 class _BoardingscreenState extends State<Boardingscreen> {
-  double scroller=0;
+  double scroller = 0;
 
   @override
   Widget build(BuildContext context) {
+    //to stop landscape mode
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown
+    ]);
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.white70,
-        title: Row(
-          mainAxisAlignment : MainAxisAlignment.start,
-        children:[
-        // Icon(
-        //     Icons.h_mobiledata,
-        //     color: Colors.blue[600],
-        //     size: 40.0,
-        //   ),
-        Text(
+        title: Row(mainAxisAlignment: MainAxisAlignment.start, children: [
+          // Icon(
+          //     Icons.h_mobiledata,
+          //     color: Colors.blue[600],
+          //     size: 40.0,
+          //   ),
+          Text(
             "H",
             style: TextStyle(
-            fontWeight:FontWeight.bold,
-            fontSize : 30.0,
-            color: Colors.teal,
+              fontWeight: FontWeight.bold,
+              fontSize: 30.0,
+              color: Colors.teal,
             ),
-        ),
-        Text(
+          ),
+          Text(
             "Bot",
             style: TextStyle(
-            fontSize : 20.0,
-            color: Colors.blue[600],
+              fontSize: 20.0,
+              color: Colors.blue[600],
             ),
-        ),
-      ]
-      ),
+          ),
+        ]),
       ),
       body: Stack(
         children: [
           PageView(
-            onPageChanged: (val){
+            onPageChanged: (val) {
               setState(() {
-                scroller=val.toDouble();
+                scroller = val.toDouble();
               });
             },
             children: [
               Padding(
                 padding: const EdgeInsets.all(8.0),
                 child: onboard(
-                  boardColumn:Column(
+                  boardColumn: Column(
                     mainAxisSize: MainAxisSize.min,
                     children: [
-                     const Text("Need a Health Support",textAlign: TextAlign.center,
-                    style: TextStyle(fontWeight: FontWeight.bold,color: Colors.black,fontSize: 30),
-                     ),
-                      const Text("Chat With Our Doctors",textAlign: TextAlign.center,
-                        style: TextStyle(fontWeight: FontWeight.bold,color: Colors.grey,fontSize: 20),
+                      const Text(
+                        "Need a Health Support",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black,
+                            fontSize: 30),
                       ),
-                      const SizedBox(height: 20,),
+                      const Text(
+                        "Chat With Our Doctors",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.grey,
+                            fontSize: 20),
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
                       SizedBox(
                         height: 300,
                         width: 300,
                         child: Image.asset('images/lp2.png'),
                       ),
-
                     ],
-                  ) ,
+                  ),
                 ),
               ),
               //onboard page 2
               //onboard page 3
               onboard(
-                boardColumn:Column(
+                boardColumn: Column(
                   mainAxisSize: MainAxisSize.min,
                   children: [
-
-                    const Text("Check yor health symptom",textAlign: TextAlign.center,
-                      style: TextStyle(fontWeight: FontWeight.bold,color: Colors.black,fontSize: 30),
+                    const Text(
+                      "Check yor health symptom",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                          fontSize: 30),
                     ),
-                    const SizedBox(height: 10,),
-                    const Text("Use our HealBot to check your health condition",textAlign: TextAlign.center,
-                      style: TextStyle(fontWeight: FontWeight.bold,color: Colors.grey,fontSize: 20),
+                    const SizedBox(
+                      height: 10,
                     ),
-                    const SizedBox(height: 20,),
+                    const Text(
+                      "Use our HealBot to check your health condition",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.grey,
+                          fontSize: 20),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
                     SizedBox(
                       height: 300,
                       width: 300,
                       child: Image.asset('images/pg2.png'),
                     ),
-
                   ],
-                ) ,
+                ),
               ),
               Padding(
                 padding: const EdgeInsets.all(8.0),
                 child: onboard(
-                  boardColumn:Column(
+                  boardColumn: Column(
                     mainAxisSize: MainAxisSize.min,
                     children: [
-                      const Text("Lets Get Started",textAlign: TextAlign.center,
-                        style: TextStyle(fontWeight: FontWeight.bold,color: Colors.black,fontSize: 30),
+                      const Text(
+                        "Lets Get Started",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black,
+                            fontSize: 30),
                       ),
                       // const Text("Chat With Our Doctors",textAlign: TextAlign.center,
                       //   style: TextStyle(fontWeight: FontWeight.bold,color: Colors.grey,fontSize: 25),
                       // ),
-                      const SizedBox(height: 20,),
+                      const SizedBox(
+                        height: 20,
+                      ),
                       SizedBox(
                         height: 300,
                         width: 300,
                         child: Image.asset('images/lp2.png'),
                       ),
-
                     ],
-                  ) ,
+                  ),
                 ),
               ),
             ],
@@ -135,64 +167,73 @@ class _BoardingscreenState extends State<Boardingscreen> {
                 DotsIndicator(
                   dotsCount: 3,
                   position: scroller,
-                  decorator: const DotsDecorator(
-                    activeColor: Colors.blue
-                  ),
+                  decorator: const DotsDecorator(activeColor: Colors.blue),
                 ),
-                scroller==2? Padding(
-                  padding: const EdgeInsets.only(left: 20,right: 20),
-                  child: ElevatedButton(style:ButtonStyle(
-                    backgroundColor: MaterialStateProperty.all(Colors.blue)
-                  ),onPressed: (){
-                    Navigator.pushAndRemoveUntil(context, MaterialPageRoute(builder: (context) => const LandingPage()),
-                        (Route <dynamic> route) => false,
-                    );
-                  },
-                  child: const Text('Lets GO')),
-                ):
-                TextButton(
-                  child: Text("Next",style: TextStyle(fontSize: 20,color: Colors.grey),),
-                  onPressed: (){},
-                ),
-                const SizedBox(height: 20,)
+                scroller == 2
+                    ? Padding(
+                        padding: const EdgeInsets.only(left: 20, right: 20),
+                        child: ElevatedButton(
+                            style: ButtonStyle(
+                                backgroundColor:
+                                    MaterialStateProperty.all(Colors.blue)),
+                            onPressed: () {
+                              Navigator.pushAndRemoveUntil(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => const LandingPage()),
+                                (Route<dynamic> route) => false,
+                              );
+                            },
+                            child: const Text('Lets GO')),
+                      )
+                    : TextButton(
+                        child: Text(
+                          "Next",
+                          style: TextStyle(fontSize: 20, color: Colors.grey),
+                        ),
+                        onPressed: () {},
+                      ),
+                const SizedBox(
+                  height: 20,
+                )
               ],
             ),
           ),
-          const SizedBox(height: 20,)
+          const SizedBox(
+            height: 20,
+          )
         ],
       ),
     );
   }
 }
+
 class onboard extends StatelessWidget {
   final Column? boardColumn;
-  const onboard({Key? key,this.boardColumn}) : super(key: key);
+  const onboard({Key? key, this.boardColumn}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Stack(
       children: [
         Container(
-          color: Colors.white70,
-          child: Center(child: boardColumn,)
-
-        ),
+            color: Colors.white70,
+            child: Center(
+              child: boardColumn,
+            )),
         Align(
           alignment: Alignment.bottomCenter,
           child: Container(
             width: MediaQuery.of(context).size.width,
             height: 100,
-            decoration:const BoxDecoration(
-              color: Colors.blueGrey,
-              borderRadius: const BorderRadius.only(
-                topLeft: Radius.circular(100),topRight: Radius.circular(100)
-              )
-            ),
+            decoration: const BoxDecoration(
+                color: Colors.blueGrey,
+                borderRadius: const BorderRadius.only(
+                    topLeft: Radius.circular(100),
+                    topRight: Radius.circular(100))),
           ),
         )
       ],
     );
   }
 }
-
-
